10b705d8968a86b75bf90b03eed6ef1842d6f653
Giovanni Toledo <giovannitoledo@id.uff.br>
2025-08-31 20:12:31 -0300

    fix: update allowed origins and favicon
    - Added Vercel deployment URL to allowed origins.
    - Replaced favicon.ico with favicon.png.

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ app = FastAPI(title="API Email Classifier com IA")
 origins = [
     "http://localhost:5173",  # frontend
     "http://127.0.0.1:5173",
+    "https://email-classifier.vercel.app"
 ]
 
 app.add_middleware(
